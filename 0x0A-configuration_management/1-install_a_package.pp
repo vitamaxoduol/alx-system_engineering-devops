c593f50747ac808283c708838de979a570f76502
vitamaxoduol <vitamaxoduol@gmail.com>
2023-01-12 01:06:37 +0300

    installing flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # installing flask from pip3
 
-class mymodule::flask {
+#class mymodule::flask {
   package { 'flask':
     ensure => '2.1.0',
     provider => 'pip3',
   }
-}
+#}
 
-node 'mynode' {
-  include mymodule::flask
-}
+#node 'mynode' {
+ # include mymodule::flask
+#}
