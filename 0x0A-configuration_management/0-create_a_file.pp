37b365d76b040059ddc79ff14366cd3283ed3d77
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-11-28 05:31:11 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#create a file in /tmp
-
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-#create a manifest that kills a process named killmenow
-
-exec { 'pkill':
-    command => 'pkill -f killmenow',
-    provider => 'shell',
-}
